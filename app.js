40ab6e5080e6d1752032659e6587a55512ffa022
bosshogg86 <jeffhogg86@gmail.com>
2020-08-13 15:59:52 -0700

    ajax returns object

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 $( document ).ready(function() {
-    
+  
+  let cityName = 'Napa'
+  
+  const APIKey = 'c82e07f31254ca48265cc271c0642dc7'
+  const queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + APIKey + '&units=imperial';
+
     $.ajax({
-        url: 'api.openweathermap.org/data/2.5/weather?q={city name}&appid=c82e07f31254ca48265cc271c0642dc7',
+        url: queryURL,
         method: 'GET',
     }).then(function(response) {
       console.log(response);
