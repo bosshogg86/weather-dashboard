6c3fbfc0fbcc45dcdc4448adfcdfdcb0e8be4b7a
bosshogg86 <jeffhogg86@gmail.com>
2020-08-14 20:36:56 -0700

    search box returns city data

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 $( document ).ready(function() {
   
-  let cityName;
+  // let cityName = '';
   
   const APIKey = 'c82e07f31254ca48265cc271c0642dc7'
-  const queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + APIKey + '&units=imperial';
+  // const queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + APIKey + '&units=imperial';
 
   
   
   $('#search-button').click(function(){
     event.preventDefault();
-    cityName = $('#search-box').val();
+    let cityName = $('#search-box').val().trim();
+    let queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + APIKey + '&units=imperial';
     console.log(cityName);
     $.ajax({
       url: queryURL,
