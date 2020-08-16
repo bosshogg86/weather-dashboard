6e347c74ada92432ba274f563485cb453a6eecc2
bosshogg86 <jeffhogg86@gmail.com>
2020-08-15 23:51:51 -0700

    renders search history

--- app.js
+++ app.js
@@ -1,85 +1,106 @@
-$( document ).ready(function() {
-  
+$(document).ready(function () {
   // let cityName = '';
   let userInputs = [];
   let userInputIndex = 0;
   // let cityName = '';
-  const $cityName = $('#search-box');
-  const APIKey = 'c82e07f31254ca48265cc271c0642dc7';
+  const $cityName = $("#search-box");
+  const APIKey = "c82e07f31254ca48265cc271c0642dc7";
   // const cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}&units=imperial`;
   // const fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIKey}&units=imperial`;
   // const uvURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${APIKey}&lat=${lat}&lon=${lon}`;
-   
+
   // Set default city
   init();
 
   // Search Box sets city
-  $('#search-button').click(function(){
+  $("#search-button").click(function () {
     event.preventDefault();
     cityName = $cityName.val();
     const cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}&units=imperial`;
     const fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIKey}&units=imperial`;
-    
+
     // City name call
     $.ajax({
       url: cityURL,
-      method: 'GET',
-    }).then(function(cityData) {
+      method: "GET",
+    }).then(function (cityData) {
       console.log(cityData);
       const lat = cityData.coord.lat;
       const lon = cityData.coord.lon;
       const uvURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${APIKey}&lat=${lat}&lon=${lon}`;
-      
-      // Nested UV call 
+
+      // Nested UV call
       $.ajax({
         url: uvURL,
-        method: 'GET',
-      }).then(function(uvData) {
+        method: "GET",
+      }).then(function (uvData) {
         console.log(uvData);
-        $('.city-card').empty();
-        const $cityName = $('<h3>').text(cityData.name);
-        const $temp = $('<p>').text('Temp: ' + cityData.main.temp + '°F');
-        const $humidity = $('<p>').text('Humidity: ' + cityData.main.humidity + '%');
-        const $speed = $('<p>').text('Wind Speed: ' + cityData.wind.speed + ' MPH');
-        const $uv = $('<p>').text('UV Index: ' + uvData.value);
-        $('.city-card').append($cityName, $temp, $humidity, $speed, $uv);
+        $(".city-card").empty();
+        const $cityName = $("<h3>").text(cityData.name);
+        const $temp = $("<p>").text("Temp: " + cityData.main.temp + "°F");
+        const $humidity = $("<p>").text(
+          "Humidity: " + cityData.main.humidity + "%"
+        );
+        const $speed = $("<p>").text(
+          "Wind Speed: " + cityData.wind.speed + " MPH"
+        );
+        const $uv = $("<p>").text("UV Index: " + uvData.value);
+        $(".city-card").append($cityName, $temp, $humidity, $speed, $uv);
       });
     });
 
     // 5 day call
     $.ajax({
       url: fiveDayURL,
-      method: 'GET',
-    }).then(function(fiveDayData) {
-      $('.weather-card').empty();
-      const day1Date = $('<h5>').text(fiveDayData.list[4].dt_txt.slice(5, 10));
-      const day2Date = $('<h5>').text(fiveDayData.list[12].dt_txt.slice(5, 10));
-      const day3Date = $('<h5>').text(fiveDayData.list[20].dt_txt.slice(5, 10));
-      const day4Date = $('<h5>').text(fiveDayData.list[28].dt_txt.slice(5, 10));
-      const day5Date = $('<h5>').text(fiveDayData.list[36].dt_txt.slice(5, 10));
-      const day1Temp = $('<p>').text('Temp: ' + fiveDayData.list[4].main.temp_max + '°F');
-      const day2Temp = $('<p>').text('Temp: ' + fiveDayData.list[12].main.temp_max + '°F');
-      const day3Temp = $('<p>').text('Temp: ' + fiveDayData.list[20].main.temp_max + '°F');
-      const day4Temp = $('<p>').text('Temp: ' + fiveDayData.list[28].main.temp_max + '°F');
-      const day5Temp = $('<p>').text('Temp: ' + fiveDayData.list[36].main.temp_max + '°F');
-      const day1Humid = $('<p>').text('Humidity: ' + fiveDayData.list[4].main.humidity + '%');
-      const day2Humid = $('<p>').text('Humidity: ' + fiveDayData.list[12].main.humidity + '%');
-      const day3Humid = $('<p>').text('Humidity: ' + fiveDayData.list[20].main.humidity + '%');
-      const day4Humid = $('<p>').text('Humidity: ' + fiveDayData.list[28].main.humidity + '%');
-      const day5Humid = $('<p>').text('Humidity: ' + fiveDayData.list[36].main.humidity + '%');
-      $('#day1').append(day1Date, day1Temp, day1Humid);
-      $('#day2').append(day2Date, day2Temp, day2Humid);
-      $('#day3').append(day3Date, day3Temp, day3Humid);
-      $('#day4').append(day4Date, day4Temp, day4Humid);
-      $('#day5').append(day5Date, day5Temp, day5Humid);
+      method: "GET",
+    }).then(function (fiveDayData) {
+      $(".weather-card").empty();
+      const day1Date = $("<h5>").text(fiveDayData.list[4].dt_txt.slice(5, 10));
+      const day2Date = $("<h5>").text(fiveDayData.list[12].dt_txt.slice(5, 10));
+      const day3Date = $("<h5>").text(fiveDayData.list[20].dt_txt.slice(5, 10));
+      const day4Date = $("<h5>").text(fiveDayData.list[28].dt_txt.slice(5, 10));
+      const day5Date = $("<h5>").text(fiveDayData.list[36].dt_txt.slice(5, 10));
+      const day1Temp = $("<p>").text(
+        "Temp: " + fiveDayData.list[4].main.temp_max + "°F"
+      );
+      const day2Temp = $("<p>").text(
+        "Temp: " + fiveDayData.list[12].main.temp_max + "°F"
+      );
+      const day3Temp = $("<p>").text(
+        "Temp: " + fiveDayData.list[20].main.temp_max + "°F"
+      );
+      const day4Temp = $("<p>").text(
+        "Temp: " + fiveDayData.list[28].main.temp_max + "°F"
+      );
+      const day5Temp = $("<p>").text(
+        "Temp: " + fiveDayData.list[36].main.temp_max + "°F"
+      );
+      const day1Humid = $("<p>").text(
+        "Humidity: " + fiveDayData.list[4].main.humidity + "%"
+      );
+      const day2Humid = $("<p>").text(
+        "Humidity: " + fiveDayData.list[12].main.humidity + "%"
+      );
+      const day3Humid = $("<p>").text(
+        "Humidity: " + fiveDayData.list[20].main.humidity + "%"
+      );
+      const day4Humid = $("<p>").text(
+        "Humidity: " + fiveDayData.list[28].main.humidity + "%"
+      );
+      const day5Humid = $("<p>").text(
+        "Humidity: " + fiveDayData.list[36].main.humidity + "%"
+      );
+      $("#day1").append(day1Date, day1Temp, day1Humid);
+      $("#day2").append(day2Date, day2Temp, day2Humid);
+      $("#day3").append(day3Date, day3Temp, day3Humid);
+      $("#day4").append(day4Date, day4Temp, day4Humid);
+      $("#day5").append(day5Date, day5Temp, day5Humid);
       // console.log(fiveDayData);
       storeInput();
       renderInputs();
-      $cityName.val('');
-      // console.log(userInputs);      
+      $cityName.val("");
+      // console.log(userInputs);
     });
-
-
   });
 
   function storeInput() {
@@ -87,86 +108,103 @@ $( document ).ready(function() {
     // $('<img>').attr('src', thumb_url);
     userInputs.push(cityName);
     console.log(userInputs);
-    localStorage.setItem('userInputArr', userInputs);
+    localStorage.setItem("userInput", userInputs);
   }
 
   function renderInputs() {
-    localStorage.getItem('userInputArr');
-    for (let i = 0; i < userInputs.length; i++) {
-      
-      const userInput = $('<li>');
-      userInput.addClass('list-group-item');
-      $('.list-group').append(userInput);
+    $(".list-group").empty();
+    userInputs.forEach(function (cityName) {
+      const userInput = $("<li>");
       userInput.text(cityName);
-      console.log(cityName);
-    }
+      userInput.addClass("list-group-item");
+      $(".list-group").append(userInput);
+    });
   }
 
   // Sets SF to default city
   function init() {
-  cityName = 'San Francisco';
-  const cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}&units=imperial`;
-  const fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIKey}&units=imperial`;
-  
-  // Default city name call
+    cityName = "San Francisco";
+    const cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}&units=imperial`;
+    const fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIKey}&units=imperial`;
+
+    // Default city name call
     $.ajax({
       url: cityURL,
-      method: 'GET',
-    }).then(function(cityData) {
+      method: "GET",
+    }).then(function (cityData) {
       console.log(cityData);
       const lat = cityData.coord.lat;
       const lon = cityData.coord.lon;
       const uvURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${APIKey}&lat=${lat}&lon=${lon}`;
-      
-      // Nested default UV call 
+
+      // Nested default UV call
       $.ajax({
         url: uvURL,
-        method: 'GET',
-      }).then(function(uvData) {
+        method: "GET",
+      }).then(function (uvData) {
         console.log(uvData);
-        $('.city-card').empty();
-        const $cityName = $('<h3>').text(cityData.name);
-        const $temp = $('<p>').text('Temp: ' + cityData.main.temp + '°F');
-        const $humidity = $('<p>').text('Humidity: ' + cityData.main.humidity + '%');
-        const $speed = $('<p>').text('Wind Speed: ' + cityData.wind.speed + ' MPH');
-        const $uv = $('<p>').text('UV Index: ' + uvData.value);
-        $('.city-card').append($cityName, $temp, $humidity, $speed, $uv);
+        $(".city-card").empty();
+        const $cityName = $("<h3>").text(cityData.name);
+        const $temp = $("<p>").text("Temp: " + cityData.main.temp + "°F");
+        const $humidity = $("<p>").text(
+          "Humidity: " + cityData.main.humidity + "%"
+        );
+        const $speed = $("<p>").text(
+          "Wind Speed: " + cityData.wind.speed + " MPH"
+        );
+        const $uv = $("<p>").text("UV Index: " + uvData.value);
+        $(".city-card").append($cityName, $temp, $humidity, $speed, $uv);
       });
     });
 
     // Default 5 day call
     $.ajax({
       url: fiveDayURL,
-      method: 'GET',
-    }).then(function(fiveDayData) {
-      $('.weather-card').empty();
-      const day1Date = $('<h5>').text(fiveDayData.list[4].dt_txt.slice(5, 10));
-      const day2Date = $('<h5>').text(fiveDayData.list[12].dt_txt.slice(5, 10));
-      const day3Date = $('<h5>').text(fiveDayData.list[20].dt_txt.slice(5, 10));
-      const day4Date = $('<h5>').text(fiveDayData.list[28].dt_txt.slice(5, 10));
-      const day5Date = $('<h5>').text(fiveDayData.list[36].dt_txt.slice(5, 10));
-      const day1Temp = $('<p>').text('Temp: ' + fiveDayData.list[4].main.temp_max + '°F');
-      const day2Temp = $('<p>').text('Temp: ' + fiveDayData.list[12].main.temp_max + '°F');
-      const day3Temp = $('<p>').text('Temp: ' + fiveDayData.list[20].main.temp_max + '°F');
-      const day4Temp = $('<p>').text('Temp: ' + fiveDayData.list[28].main.temp_max + '°F');
-      const day5Temp = $('<p>').text('Temp: ' + fiveDayData.list[36].main.temp_max + '°F');
-      const day1Humid = $('<p>').text('Humidity: ' + fiveDayData.list[4].main.humidity + '%');
-      const day2Humid = $('<p>').text('Humidity: ' + fiveDayData.list[12].main.humidity + '%');
-      const day3Humid = $('<p>').text('Humidity: ' + fiveDayData.list[20].main.humidity + '%');
-      const day4Humid = $('<p>').text('Humidity: ' + fiveDayData.list[28].main.humidity + '%');
-      const day5Humid = $('<p>').text('Humidity: ' + fiveDayData.list[36].main.humidity + '%');
-      $('#day1').append(day1Date, day1Temp, day1Humid);
-      $('#day2').append(day2Date, day2Temp, day2Humid);
-      $('#day3').append(day3Date, day3Temp, day3Humid);
-      $('#day4').append(day4Date, day4Temp, day4Humid);
-      $('#day5').append(day5Date, day5Temp, day5Humid);
+      method: "GET",
+    }).then(function (fiveDayData) {
+      $(".weather-card").empty();
+      const day1Date = $("<h5>").text(fiveDayData.list[4].dt_txt.slice(5, 10));
+      const day2Date = $("<h5>").text(fiveDayData.list[12].dt_txt.slice(5, 10));
+      const day3Date = $("<h5>").text(fiveDayData.list[20].dt_txt.slice(5, 10));
+      const day4Date = $("<h5>").text(fiveDayData.list[28].dt_txt.slice(5, 10));
+      const day5Date = $("<h5>").text(fiveDayData.list[36].dt_txt.slice(5, 10));
+      const day1Temp = $("<p>").text(
+        "Temp: " + fiveDayData.list[4].main.temp_max + "°F"
+      );
+      const day2Temp = $("<p>").text(
+        "Temp: " + fiveDayData.list[12].main.temp_max + "°F"
+      );
+      const day3Temp = $("<p>").text(
+        "Temp: " + fiveDayData.list[20].main.temp_max + "°F"
+      );
+      const day4Temp = $("<p>").text(
+        "Temp: " + fiveDayData.list[28].main.temp_max + "°F"
+      );
+      const day5Temp = $("<p>").text(
+        "Temp: " + fiveDayData.list[36].main.temp_max + "°F"
+      );
+      const day1Humid = $("<p>").text(
+        "Humidity: " + fiveDayData.list[4].main.humidity + "%"
+      );
+      const day2Humid = $("<p>").text(
+        "Humidity: " + fiveDayData.list[12].main.humidity + "%"
+      );
+      const day3Humid = $("<p>").text(
+        "Humidity: " + fiveDayData.list[20].main.humidity + "%"
+      );
+      const day4Humid = $("<p>").text(
+        "Humidity: " + fiveDayData.list[28].main.humidity + "%"
+      );
+      const day5Humid = $("<p>").text(
+        "Humidity: " + fiveDayData.list[36].main.humidity + "%"
+      );
+      $("#day1").append(day1Date, day1Temp, day1Humid);
+      $("#day2").append(day2Date, day2Temp, day2Humid);
+      $("#day3").append(day3Date, day3Temp, day3Humid);
+      $("#day4").append(day4Date, day4Temp, day4Humid);
+      $("#day5").append(day5Date, day5Temp, day5Humid);
       console.log(fiveDayData);
       // renderInputs();
     });
   }
-
-  });
-  
-
-
-  
+});
